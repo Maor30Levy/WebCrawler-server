d6694a645be8c68f21040930693deccf474fb82e
unknown <maor30levy@gmail.com>
2021-03-14 19:51:56 +0200

    3.0

--- src/keys/setKeys.js
+++ src/keys/setKeys.js
@@ -1,18 +1,18 @@
-const {saveSecret} = require('../aws/ssm');
+const { saveSecret } = require('../aws/ssm');
 const keys = require('./keys');
 
-const setKeys = async ()=>{
-    try{
+const setKeys = async () => {
+    try {
         const devPort = process.env.PORT;
         const devMongoDB = process.env.MONGODB;
-        const client = process.env.CLIENT_HOST;
-    
-        await saveSecret('serverPort',devPort);
-        await saveSecret('mongoDB',devMongoDB);
-        await saveSecret('clientHost',client);
-    }catch(err){
+        const serverHost = process.env.SERVER_HOST;
+
+        await saveSecret('serverPort', devPort);
+        await saveSecret('mongoDB', devMongoDB);
+        await saveSecret('serverHost', serverHost);
+    } catch (err) {
         console.log(err);
     }
 };
 
-module.exports = {setKeys};
+module.exports = { setKeys };
